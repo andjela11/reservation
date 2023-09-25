0e579b053cf5d4284fd5d97163295c58cf9dfbc2
andjela11 <65898647+andjela11@users.noreply.github.com>
2023-09-25 14:30:34 +0200

    Feature/add-db-context (#3)
    * add and register db context
    
    * connect context and add migrations

---
+++ Persistence/Migrations/20230925120603_InitialMigration.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Reservations",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    MovieId = table.Column<int>(type: "int", nullable: false),
+                    SeatNumbers = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Reservations", x => x.Id);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.InsertData(
+                table: "Reservations",
+                columns: new[] { "Id", "MovieId", "SeatNumbers" },
+                values: new object[,]
+                {
+                    { 1, 1, 250 },
+                    { 2, 2, 250 },
+                    { 3, 3, 250 },
+                    { 4, 4, 250 },
+                    { 5, 5, 250 },
+                    { 6, 6, 250 },
+                    { 7, 7, 250 },
+                    { 8, 8, 250 },
+                    { 9, 9, 250 },
+                    { 10, 10, 250 }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Reservations");
+        }
+    }
+}
---
+++ Persistence/PersistenceServiceCollectionExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Persistence;
+
+public static class PersistenceServiceCollectionExtensions
+{
+    public static IServiceCollection AddPersistenceServices(this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddDbContext<DataContext>(opt =>
+        {
+            opt.UseMySQL(configuration.GetConnectionString("Default"),
+                x => x.MigrationsAssembly(Assembly.GetExecutingAssembly().ToString()));
+        });
+
+        return services;
+    }
+}
