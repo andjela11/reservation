5569f2e5f204155798ba47e09ff4f47d03c61063
andjela11 <65898647+andjela11@users.noreply.github.com>
2023-09-27 11:37:45 +0200

    Feature/delete-reservation (#8)
    * add delete reservation feature
    
    * remove comment
    
    * format code
    
    * remove unit from command handler signature

---
+++ Application/Features/Commands/DeleteReservation/DeleteReservationCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using Application.Exceptions;
+using Application.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Features.Commands.DeleteReservation;
+
+public sealed class DeleteReservationCommandHandler : IRequestHandler<DeleteReservationCommand>
+{
+    private readonly IDataContext _context;
+
+    public DeleteReservationCommandHandler(IDataContext context)
+    {
+        _context = context;
+    }
+
+    public async Task Handle(DeleteReservationCommand request, CancellationToken cancellationToken)
+    {
+        var reservation = await _context.Reservations
+                    .SingleOrDefaultAsync(x => x.Id == request.Id, cancellationToken);
+
+        if (reservation is null)
+        {
+            throw new EntityNotFoundException($"Entity with Id {request.Id} doesn't exist");
+        }
+
+        _context.Reservations.Remove(reservation);
+        await _context.SaveChangesAsync(cancellationToken);
+    }
+}
