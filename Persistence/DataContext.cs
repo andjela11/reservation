f85ae5bc651b1524ab942fca46c85e275c1dfb29
andjela.jovanovic <andjela1198@gmail.com>
2023-10-02 15:58:41 +0200

    add timestamps

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -20,11 +20,43 @@ public class DataContext : DbContext, IDataContext
             {
                 Id = i + 1,
                 MovieId = i + 1,
-                SeatNumbers = 250
+                SeatNumbers = 250,
+                CreatedAt = DateTime.Now,
+                UpdatedAt = DateTime.Now
             };
             reservations.Add(reservation);
         }
 
         modelBuilder.Entity<Reservation>().HasData(reservations);
+    }
+
+    public override int SaveChanges()
+    {
+        AddTimestamps();
+        return base.SaveChanges();
+    }
+    
+    public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+    {
+        AddTimestamps();
+        return await base.SaveChangesAsync(cancellationToken);
+    }
+
+    private void AddTimestamps()
+    {
+        var entities = ChangeTracker.Entries()
+            .Where(x => x.Entity is BaseEntity && (x.State == EntityState.Added || x.State == EntityState.Modified));
+
+        foreach (var entity in entities)
+        {
+            var dateNow = DateTime.UtcNow;
+
+            if (entity.State == EntityState.Added)
+            {
+                ((BaseEntity)entity.Entity).CreatedAt = dateNow;
+            }
+
+            ((BaseEntity)entity.Entity).UpdatedAt = dateNow;
+        }
     }
 }
